3714057939625b7e69152def15389314114489de
Naim Malek <naimmalek7600@gmail.com>
2019-11-15 23:42:16 +0530

    changes

--- assets/js/resume.js
+++ assets/js/resume.js
@@ -1,8 +1,9 @@
-(function($) {
-  "use strict"; // Start of use strict
+
+setTimeout(()=>{ 
 
   // Smooth scrolling using jQuery easing
   $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
+    
     if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
       var target = $(this.hash);
       target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
@@ -16,7 +17,7 @@
   });
 
   // Closes responsive menu when a scroll trigger link is clicked
-  $('.js-scroll-trigger').click(function() {
+  $('.js-scroll-trigger').click(()=> {
     $('.navbar-collapse').collapse('hide');
   });
 
@@ -25,4 +26,6 @@
     target: '#sideNav'
   });
 
-})(jQuery); // End of use strict
+}, 2000);
+
+
